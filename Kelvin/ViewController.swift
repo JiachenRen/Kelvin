8e4fb2d7e18eeb87b702c10b5cb633011b9f0e68
jiachenren <arc4jiachenren@gmail.com>
2018-11-04 11:06:33 -0500

    Initial Commit

---
+++ Kelvin/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kelvin
+//
+//  Created by Jiachen Ren on 11/4/18.
+//  Copyright © 2018 Jiachen Ren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
