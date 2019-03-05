ae2a075b4a0e4ccafc663031baab8992a66bc2b1
jiachenren <arc4jiachenren@gmail.com>
2019-03-05 00:28:05 -0500

    Merge header files and plist for iOS and macOS frameworks

---
+++ Kelvin/Kelvin.h
@@ -0,0 +1,12 @@
+//
+//  Kelvin.h
+//  Kelvin
+//
+//  Created by Jiachen Ren on 3/5/19.
+//  Copyright © 2019 Jiachen Ren. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double KelvinVersionNumber;
+FOUNDATION_EXPORT const unsigned char KelvinVersionString[];
